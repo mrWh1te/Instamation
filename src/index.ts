bb81b316ab5f511b6a4a0a697c762a41464d54ec
Michael Lage <gs.urmomma@gmail.com>
2020-01-21 14:42:06 -0700

    WIP Instamation Class
    - async static constructor method
    - laying ground work for chaining calls

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@
 import 'module-alias/register'
 import puppeteer from 'puppeteer'
 
-import { login } from '@helpers/instagram/auth'
-import { closeTurnOnNotificationsModalIfOpen } from '@helpers/instagram/modals';
+import { Instamation } from '@instamation'
 
 // Main Script
 (async () => {
@@ -14,21 +13,14 @@ import { closeTurnOnNotificationsModalIfOpen } from '@helpers/instagram/modals';
   try {
     // Start Puppeteer
     browser = await puppeteer.launch({headless: false})
-    const pages: puppeteer.Page[] = await browser.pages()
-    const page: puppeteer.Page = pages.length === 0 ? await browser.newPage() : pages[0]
 
-    // TODO: load cookies
-    // TODO: load db
+    // Star the Instagram bot
+    const bot = await Instamation.asyncConstructor(browser, {auth: {username: '', password: ''}})
 
-    // Login to Instagram
-    await login(page) // TODO: check if not authenticated
-
-    // TODO: save cookies
-    
-    // Deal with the "Turn on Notifications" modal if it opens
-    await closeTurnOnNotificationsModalIfOpen(page) // TODO: only check after logging in
-
-    // Process feed (liking photos based on critieria)
+    // Run it
+    // bot
+    //   .feed()
+    //   .favoriteAllFrom()
 
   } catch (error) {
     console.error(error)
--- src/instamation/index.ts
+++ src/instamation/index.ts
@@ -4,11 +4,49 @@
 
 import puppeteer from 'puppeteer'
 
+import { login } from '@helpers/instagram/auth';
+import { closeTurnOnNotificationsModalIfOpen } from '@helpers/instagram/modals';
+
 import { InstamationOptions } from './interfaces/instamation-options.interface';
 
-module.exports = async (browser: puppeteer.Browser, options: InstamationOptions) => {
-  
+export class Instamation {
+  // Main web page that Puppeteer is managing in Chrome
+  private activePage: puppeteer.Page | null = null
 
-  
+  constructor() {}
+  public static asyncConstructor = async (browser: puppeteer.Browser, options: InstamationOptions) => {
+    const bot = new Instamation()
+    await bot.setup(browser, options)
 
+    return bot
+  }
+
+  public async setup(browser: puppeteer.Browser, options: InstamationOptions) {
+    // Setup
+    const pages = await browser.pages()
+    this.activePage = pages.length === 0 ? await browser.newPage() : pages[0]
+
+    // TODO: load cookies
+    // TODO: load db
+
+    // TODO: check if not authenticated
+    // Login to Instagram
+    await login(this.activePage) 
+
+    // TODO: save cookies
+    
+    // Deal with the "Turn on Notifications" modal if it opens
+    await closeTurnOnNotificationsModalIfOpen(this.activePage) // TODO: only check after logging in
+  }
+
+  //
+  // Auth
+
+
+  //
+  // Process feed (liking photos based on critieria)
+  feed() {
+
+    return this
+  }
 }
