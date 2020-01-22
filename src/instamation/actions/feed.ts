e32f5bf21b118a5eb08f31907410a9c6f073429d
Michael Lage <gs.urmomma@gmail.com>
2020-01-21 22:42:22 -0700

    Added InstamationAction type to missing undefined Action

--- src/instamation/actions/feed.ts
+++ src/instamation/actions/feed.ts
@@ -1,10 +1,12 @@
 import puppeteer from 'puppeteer'
 
+import { InstamationAction } from './instamation-action.interface'
+
 /**
  * @description   Pipeable methods for crawling/interacting with the main feed page in Instagram
  * @param usernames
  */
 
-export const favoriteAllFrom = (...usernames: string[]) => async(page: puppeteer.Page) => {
+export const favoriteAllFrom: InstamationAction = (...usernames: string[]) => async(page: puppeteer.Page) => {
   console.log(`favorite all from ${usernames.join(', ')}`)
 }
